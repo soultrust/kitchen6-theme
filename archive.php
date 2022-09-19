d9cf18fa2eff9b1b2a881dfe3531c9c8e1aefc8f
Tomo Suzuki <tomo@soultrust.com>
2022-09-18 20:23:10 -0400

    Make tag archive pages show content

--- archive.php
+++ archive.php
@@ -2,14 +2,30 @@
   get_header();
 ?>
 <main id="primary" class="site-main">
+<h2 class="archive-title"><?php the_archive_title(); ?></h2>
 	<?php
 
-	while ( have_posts() ) {
-		the_post(); ?>
-		<h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
-		<?php }
-    echo paginate_links(); ?>
+$tag_args = array(
+	'post_type' => 'recipe',
+	'tag' => single_tag_title('', false)
+);
+$category_args = array(
+	'post_type' => 'recipe',
+	'category' => single_cat_title('', false)
+);
+
+if (is_tag()) {
+	$recipes = new WP_Query($tag_args);
+}
+if (is_category()) {
+	$recipes = new WP_Query($category_args);
+}
+
+while ($recipes->have_posts()) {
+	$recipes->the_post(); ?>
+	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+<?php 
+} wp_reset_postdata(); ?>
 </main>
 <?php
 	get_footer();
