f44c0bdd0cf49c85fb24d5688067c05258973e37
Sudo Suzuki <>
2025-09-29 06:23:31 -0400

    Make single recipe page usable

--- header.php
+++ header.php
@@ -8,7 +8,8 @@
   <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/kitchen6/favicons/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/kitchen6/favicons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/kitchen6/favicons/favicon-16x16.png">
-  <link rel="manifest" href="/wp-content/themes/kitchen6/site.webmanifest">
+  <!-- <link rel="manifest" href="/wp-content/themes/kitchen6/site.webmanifest"> -->
+  <link rel="manifest" href="/manifest.json">
   <link rel="mask-icon" href="/wp-content/themes/kitchen6/favicons/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
@@ -20,12 +21,12 @@
 
     <?php if (is_front_page()) { ?>
     <div class="site-title">
-      <?php echo file_get_contents(get_stylesheet_directory_uri() . '/images/kitchen6-logo.svg') ?>
+      <?php echo file_get_contents(get_stylesheet_directory_uri() . '../images/kitchen6-logo.svg') ?>
     </div>
     <?php } else { ?>
     <div class="site-title">
       <a href="<?php echo esc_url(home_url('/')); ?>">
-        <?php echo file_get_contents(get_stylesheet_directory_uri() . '/images/kitchen6-logo.svg') ?>
+        <?php echo file_get_contents(get_stylesheet_directory_uri() . '../images/kitchen6-logo.svg') ?>
       </a>
     </div>
     <?php } ?>
